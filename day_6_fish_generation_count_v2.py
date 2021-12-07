942abf695b98a062ee405554495166d28c7e83a5
ClothoMoirai <OhDaniGal@gmail.com>
2021-12-06 19:47:50 -0600

    day 6 part 2: refactor to perform reasonably with 256 days.

---
+++ day_6_fish_generation_count_v2.py
@@ -0,0 +1,37 @@
+from typing import List
+import re
+
+generations = 256
+input_file = 'day_6.txt'
+
+def get_input(myfile: str) -> list:
+    with open(myfile, 'r') as fishfile:
+        fishlist = [int(x) for x in re.sub('\n', '', fishfile.read()).split(',')]
+    fishfile.close()
+    return fishlist
+
+
+def compute_generations(fishlist: list, generations: int) -> list:
+    for i in range(generations):
+        birthing = fishlist.pop(0)
+        fishlist.append(birthing)
+        fishlist[6] += birthing
+    return fishlist
+
+
+def process_input(fishinput: list,) -> list:
+    fishlist = []
+    for i in range(9):
+        fishlist.append(0)
+    for i in fishinput:
+        fishlist[i] += 1
+    return fishlist
+
+def main(generations: int, input_file: str) -> None:
+    fishinput = get_input(input_file)
+    fishlist = process_input(fishinput)
+    fishlist = compute_generations(fishlist, generations)
+    print(sum(fishlist))
+
+
+main(generations, input_file)
